8b20d7ab40749b737ca80a832b5ec789077a1036
Sam Rankin <sam.clidigital@gmail.com>
2015-09-28 15:13:07 -0700

    Fixed up menu divided template part

--- templates/menu-divided.php
+++ templates/menu-divided.php
@@ -9,16 +9,11 @@
 
 <header id="header" class="<?php echo $header_in_grid; ?>">
   <div class="header-inner">
+  <?php wp_nav_menu( array( 'theme_location' => 'divided-left-menu', 'container_class' => 'left-side-menu',) ); ?>
   <div class="site-logo" itemtype="http://schema.org/LocalBusiness"> 
     <a itemprop="url" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php esc_attr_e( get_bloginfo( 'name' ), 'blankslate' ); ?>" rel="home"><img src="<?php if ($logo) { echo $logo; }; ?>" alt="<?php esc_attr_e( get_bloginfo( 'name' ), 'blankslate' ); ?> Logo" itemprop="logo" class="site-main-logo"/></a>
   </div>
-  <div class="menu-button-area<?php echo $hide_button; ?>">
-    <button class="menu-button">
-      <span>toggle menu</span>
-    </button>
-    <span class="menu-button-txt">Menu</span>
-  </div>
-  <?php wp_nav_menu( array( 'theme_location' => 'main-menu', 'container_class' => $hide_menu,) ); ?>
+  <?php wp_nav_menu( array( 'theme_location' => 'divided-right-menu', 'container_class' => 'right-side-menu',) ); ?>
   </div>
   
 </header>
