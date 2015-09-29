d0628e0bb3fb47cd593c1592a1f1c431ac6e03e2
Sam Rankin <sam.clidigital@gmail.com>
2015-09-29 10:36:22 -0700

    Fixed site header logos

--- templates/menu-right.php
+++ templates/menu-right.php
@@ -10,7 +10,7 @@
 
 <header id="header" class="<?php echo $header_in_grid; ?>">
   <div class="header-inner">
-  <div class="site-logo" itemtype="http://schema.org/LocalBusiness"> 
+  <div class="site-logo" itemtype="http://schema.org/LocalBusiness" <?php if ($desktop_logo_maximum_width) { echo 'style="max-width: ' . $desktop_logo_maximum_width . 'px;"'; } ?>> 
     <a itemprop="url" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php esc_attr_e( get_bloginfo( 'name' ), 'blankslate' ); ?>" rel="home" <?php if ($logo) { echo 'style="background: url(' . $logo . ') center no-repeat; background-size: contain;'; if ($desktop_logo_maximum_width) { echo ' max-width: ' . $desktop_logo_maximum_width . 'px;"'; } else {echo '"';}}; ?>><img src="<?php if ($logo) { echo $logo; }; ?>" alt="<?php esc_attr_e( get_bloginfo( 'name' ), 'blankslate' ); ?> Logo" itemprop="logo" class="site-main-logo"/></a>
   </div>
   <div class="menu-button-area<?php echo $hide_button; ?>">
