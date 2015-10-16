6ff28e7a53ad15b78e1b8052d683c20be0f1350c
Sam Rankin <sam.clidigital@gmail.com>
2015-10-16 11:44:08 -0700

    Fixed button order

--- nav-below-single.php
+++ nav-below-single.php
@@ -5,9 +5,9 @@ $prevsection = get_field( 'post_section', $prev_post->ID );
 if (!empty( $prev_post )): ?>
  <a href="<?php echo get_permalink( $prev_post->ID ); ?>" class="previous-post btn"><i class="fa fa-angle-left"></i> <span class="nav-txt">previous post</span></a>
 <?php endif; ?>
-</nav>
 <?php
 $next_post = get_next_post();
 if ( is_a( $next_post , 'WP_Post' ) ) { $nextsection = get_field( 'post_section', $next_post->ID );?>
   <a href="<?php echo get_permalink( $next_post->ID ); ?>" class="next-post btn"><span class="nav-txt">next post</span> <i class="fa fa-angle-right"></i></a>
 <?php } ?>
+</nav>
