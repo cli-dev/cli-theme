912d6822e102333276f68944af6b61f98ce95348
Sam Rankin <sam.clidigital@gmail.com>
2015-10-27 10:11:47 -0700

    Removed unnecessary styling for position grid. Added more options to position grid

--- inc/job_position.php
+++ inc/job_position.php
@@ -1,6 +1,4 @@
-<?php
-
-if( function_exists('acf_add_local_field_group') ):
+<?php if( function_exists('acf_add_local_field_group') ):
 
 acf_add_local_field_group(array (
 	'key' => 'group_55cd095785697',
@@ -23,7 +21,7 @@ acf_add_local_field_group(array (
 			'placeholder' => '',
 			'maxlength' => '',
 			'rows' => '',
-			'new_lines' => 'wpautop',
+			'new_lines' => '',
 			'readonly' => 0,
 			'disabled' => 0,
 		),
@@ -145,28 +143,12 @@ acf_add_local_field_group(array (
 			),
 		),
 	),
-	'menu_order' => 0,
+	'menu_order' => 1,
 	'position' => 'acf_after_title',
 	'style' => 'default',
 	'label_placement' => 'top',
 	'instruction_placement' => 'label',
-	'hide_on_screen' => array (
-		0 => 'permalink',
-		1 => 'the_content',
-		2 => 'excerpt',
-		3 => 'custom_fields',
-		4 => 'discussion',
-		5 => 'comments',
-		6 => 'revisions',
-		7 => 'slug',
-		8 => 'author',
-		9 => 'format',
-		10 => 'page_attributes',
-		11 => 'featured_image',
-		12 => 'categories',
-		13 => 'tags',
-		14 => 'send-trackbacks',
-	),
+	'hide_on_screen' => '',
 	'active' => 1,
 	'description' => '',
 ));
--- templates/rowlayout-positionsgrid.php
+++ templates/rowlayout-positionsgrid.php
@@ -7,15 +7,26 @@ $postid = get_the_ID();
 
 $item_id = (is_blog()) ? $page_for_posts : $postid;
 
-$first_block_background_color = (get_sub_field('first_block_background_color', $item_id)) ? 'background-color: ' . get_sub_field('first_block_background_color', $item_id) . '; ' : '';
-$first_block_text_color = (get_sub_field('first_block_text_color', $item_id)) ? 'color: ' . get_sub_field('first_block_text_color', $item_id) . '; ' : '';
-$block_background_color = (get_sub_field('block_background_color', $item_id)) ? 'background-color: ' . get_sub_field('block_background_color', $item_id) . '; ' : '';
-$block_text_color = (get_sub_field('block_text_color', $item_id)) ?  'color: ' . get_sub_field('block_text_color', $item_id) . '; ' : '';
-$first_block_intro_text = (get_sub_field('first_block_intro_text', $item_id)) ? get_sub_field('first_block_intro_text', $item_id) : '';
-$extra_class = (get_sub_field('extra_class', $item_id)) ? get_sub_field('extra_class', $item_id) : '';
+$display_intro_block = get_sub_field('display_intro_block', $item_id);
 
-$first_block_styles = 'style="' . $first_block_background_color . $first_block_text_color . '"';
-$block_styles = 'style="' . $block_background_color . $block_text_color . '"';
+$first_block_background_color = (get_sub_field('first_block_background_color', $item_id)) ? ' style="background-color: ' . get_sub_field('first_block_background_color', $item_id) . ';"' : '';
+$first_block_text_color = (get_sub_field('first_block_text_color', $item_id)) ? ' style="color: ' . get_sub_field('first_block_text_color', $item_id) . ';"' : '';
+$first_block_intro_text = get_sub_field('first_block_intro_text', $item_id);
+
+
+$block_background_color = (get_sub_field('block_background_color', $item_id)) ? ' style="background-color: ' . get_sub_field('block_background_color', $item_id) . ';"' : '';
+$block_title_color = (get_sub_field('block_title_color', $item_id)) ?  ' style="color: ' . get_sub_field('block_title_color', $item_id) . ';"' : '';
+$block_text_color = (get_sub_field('block_text_color', $item_id)) ?  ' style="color: ' . get_sub_field('block_text_color', $item_id) . ';"' : '';
+$extra_class = (get_sub_field('extra_class', $item_id)) ? get_sub_field('extra_class', $item_id) : '';
+$number_of_columns = 'style="width: ' . get_sub_field('number_of_columns', $item_id) . ';"';
+
+$display_button = get_sub_field('display_button', $item_id);
+$button_text = get_sub_field('button_text', $item_id);
+$button_class = (get_sub_field('button_class', $item_id)) ? ' ' . get_sub_field('button_class', $item_id) : '';
+
+$button = ($display_button == 1) ? '<div class="btn' . $button_class . '"">' . $button_text . '</div>' : '';
+
+$first_block = ($display_intro_block == 1) ? '<div class="position-block"' . $number_of_columns . '><div class="position-block-inner"' . $first_block_background_color . '><h2'. $first_block_text_color. '>' . $first_block_intro_text . '</h2></div></div>' : '';
 
 ?>
 
@@ -29,22 +40,19 @@ $query1 = new WP_Query( $args1 );
 
 if ( $query1->have_posts() ) : ?>
   <div class="positions">
-    <div class="position-block">
-      <div class="position-block-inner" <?php echo $first_block_styles; ?>>
-        <h2><?php echo $first_block_intro_text; ?></h2>
-      </div>
-    </div>
+    <?php echo $first_block; ?>
     <?php while ( $query1->have_posts() ) : $query1->the_post(); ?>
       <?php
         $description = get_field('description');
       ?>
-      <a href="<?php the_permalink(); ?>" class="position-block">
-      <div class="position-block-inner <?php echo $extra_class; ?>" <?php echo $block_styles; ?>>
-        <h3><?php the_title(); ?></h3>
-        <div class="job-description">
-          <?php echo $description; ?>
+      <a href="<?php the_permalink(); ?>" class="position-block" <?php echo $number_of_columns; ?>>
+        <div class="position-block-inner <?php echo $extra_class; ?>" <?php echo $block_background_color; ?>>
+          <h3<?php echo $block_title_color; ?>><?php the_title(); ?></h3>
+          <div class="job-description">
+            <p<?php echo $block_text_color; ?>><?php echo $description; ?></p>
+          </div>
+          <?php echo $button; ?>
         </div>
-      </div>
       </a>
     <?php endwhile; ?>
   </div>
