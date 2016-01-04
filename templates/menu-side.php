1e7790dda80bc32a71afd43b47b9189b2c45f162
Sam Rankin <sam.clidigital@gmail.com>
2016-01-04 13:41:30 -0700

    Added link text span wrap to side menu links

--- templates/menu-side.php
+++ templates/menu-side.php
@@ -1,4 +1,4 @@
-<?php wp_nav_menu( array( 'theme_location' => 'main-menu',) ); ?>
+<?php wp_nav_menu( array( 'theme_location' => 'main-menu', 'link_before' => '<span class="link-text">', 'link_after' => '</span>') ); ?>
   <?php if ( is_active_sidebar( 'side-menu-bottom-widget' ) ) : ?>  
     <div id="side-widget-area">  
       <?php dynamic_sidebar( 'side-menu-bottom-widget' ); ?>
