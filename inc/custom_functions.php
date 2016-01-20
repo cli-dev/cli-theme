6aa7e0f9b35306494cc8f1d2a998b91137243a82
Sam Rankin <sam.clidigital@gmail.com>
2016-01-20 15:48:23 -0700

    Fixed fancybox css

--- inc/custom_functions.php
+++ inc/custom_functions.php
@@ -959,3 +959,12 @@ function my_acf_admin_enqueue_scripts() {
 }
 
 add_action( 'acf/input/admin_enqueue_scripts', 'my_acf_admin_enqueue_scripts' );
+
+// remove wp version param from any enqueued scripts
+function vc_remove_wp_ver_css_js( $src ) {
+  if ( strpos( $src, 'ver=' ) )
+      $src = remove_query_arg( 'ver', $src );
+  return $src;
+}
+add_filter( 'style_loader_src', 'vc_remove_wp_ver_css_js', 9999 );
+add_filter( 'script_loader_src', 'vc_remove_wp_ver_css_js', 9999 );
--- templates/site-header.php
+++ templates/site-header.php
@@ -63,3 +63,4 @@
       </div>
     </header>
 <?php } ?>
+
