b839c88513adcc11a7eb1c661da6e63edac7d582
Sam Rankin <sam.clidigital@gmail.com>
2016-01-27 12:14:33 -0700

    Added custom class to col item rather than image wrapper

--- templates/rowlayout-singleimg.php
+++ templates/rowlayout-singleimg.php
@@ -10,7 +10,7 @@
   $display_title = (get_sub_field('display_title', $item_id) == 1) ? '<div class="single-image-title">' . $image['title'] . '</div>' : '';
   $width = (get_sub_field('single_image_size', $item_id)) ? ' width="' . get_sub_field('single_image_size', $item_id) . '"' : '';
   
-  $layout_classes = 'class="single-image-wrapper ' . $title_position . $custom_class . '"';
+  $layout_classes = 'class="single-image-wrapper ' . $title_position . '"';
  
 $item_add_animation = get_sub_field('add_item_animation', $item_id);
 $animation_class = ($item_add_animation == 1) ? ' wow' : '';
@@ -21,7 +21,7 @@ $item_animation_offset =  (get_sub_field('item_animation_offset', $item_id)) ? '
 
 $animation = ($item_add_animation == 1) ? $item_animation_duration . $item_animation_delay . $item_animation_offset : '';
 ?>
-<div class="col-item<?php echo $animation_class . $item_animation_effect; ?>"<?php echo $animation;?>>
+<div class="col-item<?php echo $custom_class . $animation_class . $item_animation_effect; ?>"<?php echo $animation;?>>
 <div <?php echo $layout_classes; ?>>
   <img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" <?php echo $width; ?> style="height: auto;" class="single-image" />
   <?php echo $display_title ?>
