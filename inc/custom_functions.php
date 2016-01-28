0e7b0787cec8f27e548a819bed4d5e4a201bc0e9
Sam Rankin <sam.clidigital@gmail.com>
2016-01-28 08:43:12 -0700

    Fixed site so that php errors only display on Admin side

--- inc/custom_functions.php
+++ inc/custom_functions.php
@@ -32,7 +32,7 @@ if(!function_exists('cli_is_css_folder_writable')) {
 	 * @uses is_writable()
 	 */
 	function cli_is_css_folder_writable() {
-		$css_dir = get_template_directory().'/css';
+		$css_dir = CLI_ROOT .'/css';
 
 		return is_writable($css_dir);
 	}
@@ -41,7 +41,7 @@ if(!function_exists('cli_is_css_folder_writable')) {
 function cli_generate_dynamic_css_and_js() {
 
 		if(cli_is_css_folder_writable()) {
-			$css_dir = get_template_directory().'/css/';
+			$css_dir = CLI_ROOT .'/css/';
 
 			ob_start();
 			include_once('css/style_dynamic.php');
@@ -253,6 +253,9 @@ function displayEmail() {
 }
 
 function displaySocialProfiles($icon_type, $extra_class) {
+
+  $icon_type = 'icon1';
+  $extra_class = '';
   
   $custom_class = $extra_class;
   $type_of_icon = $icon_type;
@@ -968,3 +971,39 @@ function vc_remove_wp_ver_css_js( $src ) {
 }
 add_filter( 'style_loader_src', 'vc_remove_wp_ver_css_js', 9999 );
 add_filter( 'script_loader_src', 'vc_remove_wp_ver_css_js', 9999 );
+
+function admin_alert_errors($errno, $errstr, $errfile, $errline){
+ 
+    $errorType = array (
+         E_ERROR                            => 'ERROR',
+         E_CORE_ERROR           => 'CORE ERROR',
+         E_COMPILE_ERROR        => 'COMPILE ERROR',
+         E_USER_ERROR           => 'USER ERROR',
+         E_RECOVERABLE_ERROR  => 'RECOVERABLE ERROR',
+         E_WARNING              => 'WARNING',
+         E_CORE_WARNING         => 'CORE WARNING',
+         E_COMPILE_WARNING      => 'COMPILE WARNING',
+         E_USER_WARNING         => 'USER WARNING',
+         E_NOTICE               => 'NOTICE',
+         E_USER_NOTICE          => 'USER NOTICE',
+         E_DEPRECATED                   => 'DEPRECATED',
+         E_USER_DEPRECATED      => 'USER_DEPRECATED',
+         E_PARSE                => 'PARSING ERROR'
+    );
+     
+    if (array_key_exists($errno, $errorType)) {
+        $errname = $errorType[$errno];
+    } else {
+        $errname = 'UNKNOWN ERROR';
+    }
+ob_start();?>
+<div class="error">
+  <p>
+    <strong><?php echo $errname; ?> Error: [<?php echo $errno; ?>] </strong><?php echo $errstr; ?><strong> <?php echo $errfile; ?></strong> on line <strong><?php echo $errline; ?></strong>
+  <p/>
+</div>
+<?php
+echo ob_get_clean();
+}
+ 
+set_error_handler("admin_alert_errors", E_ERROR ^ E_CORE_ERROR ^ E_COMPILE_ERROR ^ E_USER_ERROR ^ E_RECOVERABLE_ERROR ^  E_WARNING ^  E_CORE_WARNING ^ E_COMPILE_WARNING ^ E_USER_WARNING ^ E_NOTICE ^  E_USER_NOTICE ^ E_DEPRECATED    ^  E_USER_DEPRECATED    ^  E_PARSE );
