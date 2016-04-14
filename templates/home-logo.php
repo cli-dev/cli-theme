b6ab97861df36effe0fd39e3c04dcc6c75f7e261
Sam Rankin <sam.clidigital@gmail.com>
2016-04-14 12:19:29 -0700

    Logo link shortcode fix

--- templates/home-logo.php
+++ templates/home-logo.php
@@ -5,10 +5,12 @@
   $logosvg = $myoptions['svg_desktop_logo'];
   $logo = ($logosvg) ? $logosvg : $logoimg;
 
+  $logo_bg = ($logo) ? ' style="background: url(' . $logo . ') center no-repeat; background-size: contain;"' : '';
+
 ?>
 
 <div class="site-logo" itemtype="http://schema.org/LocalBusiness"> 
-   <a itemprop="url" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php esc_attr_e( get_bloginfo( 'name' ), 'cli-theme' ); ?>" rel="home" <?php if ($logo) { echo 'style="background: url(' . $logo . ') center no-repeat; background-size: contain;"';}; ?>>
-    <img src="<?php if ($logoimg) { echo $logoimg; }; ?>" alt="<?php esc_attr_e( get_bloginfo( 'name' ), 'cli-theme' ); ?> Logo" itemprop="logo" class="site-main-logo"/>
+  <a itemprop="url" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php esc_attr_e( get_bloginfo( 'name' ), 'cli-theme' ); ?>" rel="home"<?php echo $logo_bg; ?>>
+    <img src="<?php if ($logoimg) { echo $logoimg; }; ?>" alt="<?php esc_attr_e( get_bloginfo( 'name' ), 'cli-theme' ); ?> Logo" itemprop="logo" class="site-main-logo" />
   </a>
 </div>
