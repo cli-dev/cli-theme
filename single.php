8247469a70517cfa122dc8b156171c0d236ed3ae
Sam Rankin <sam.clidigital@gmail.com>
2016-04-22 08:54:39 -0700

    Slight change to single post structure

--- single.php
+++ single.php
@@ -7,11 +7,17 @@ $thumb_url = $thumb_url_array[0];
 $placeholder_img = get_field('background_image', $page_for_posts);
 ?>
 <section id="content" role="main">
-<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-<header class="post-image-window" style=" background-image: url(<?php if ( has_post_thumbnail() ) { echo $thumb_url; } else { echo $placeholder_img; }  ?>); background-position: 50% 70%; background-repeat: no-repeat; background-attachment: fixed; background-size: cover;"><?php if ( has_post_thumbnail() ) { the_post_thumbnail('medium'); } ?></header>
-<section class="entry-content">
-<?php get_template_part( 'entry', 'content' ); ?>
-</section>
-<?php endwhile; endif; ?>
+  <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+    <article id="post-<?php the_ID(); ?>" <?php post_class('single-post-wrapper'); ?>>
+      <header class="post-image-window" style=" background-image: url(<?php if ( has_post_thumbnail() ) { echo $thumb_url; } else { echo $placeholder_img; }  ?>); background-position: 50% 70%; background-repeat: no-repeat; background-attachment: fixed; background-size: cover;"><?php 
+        $title = get_the_title();
+        $attr = array('class' => "post-img-inner", 'alt' => $title,);
+        the_post_thumbnail( 'medium', $attr );
+        ?></header>
+      <section class="entry-content">
+        <?php get_template_part( 'entry', 'content' ); ?>
+      </section>
+    </article>
+  <?php endwhile; endif; ?>
 </section>
 <?php get_footer(); ?>
