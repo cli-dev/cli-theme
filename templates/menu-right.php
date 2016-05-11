edd1c4a362be4296b942f2e0b898d78cb85bd154
Sam Rankin <sam.clidigital@gmail.com>
2016-05-11 13:58:29 -0700

    Small grid changes

--- templates/menu-right.php
+++ templates/menu-right.php
@@ -42,7 +42,7 @@ if ($sticky_header == "1") {?>
 <?php } ?>
 <nav class="desktop-menu">
  <div class="site-logo" itemtype="http://schema.org/LocalBusiness"<?php if ($desktop_logo_maximum_width) { echo ' style="max-width: ' . $desktop_logo_maximum_width . 'px;"'; } ?>> 
-   <a itemprop="url" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php esc_attr_e( get_bloginfo( 'name' ), 'cli-theme' ); ?>" rel="home" <?php if ($logo) { echo 'style="background: url(' . $logo . ') center no-repeat; background-size: contain;"';}; ?>>
+   <a itemprop="url" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php esc_attr_e( get_bloginfo( 'name' ), 'cli-theme' ); ?>" rel="home" <?php if ($logo) { echo 'style="background: url(' . $logo . ') left center no-repeat; background-size: contain;"';}; ?>>
     <img src="<?php if ($logoimg) { echo $logoimg; }; ?>" alt="<?php esc_attr_e( get_bloginfo( 'name' ), 'cli-theme' ); ?> Logo" itemprop="logo" class="site-main-logo"/>
   </a>
 </div>
