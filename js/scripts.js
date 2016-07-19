7a30c1ea0495d01673ee4e786bdf6b31cfa4db1d
Sam Rankin <sam.clidigital@gmail.com>
2016-07-19 15:01:19 -0700

    Midnight plugin added

--- js/scripts.js
+++ js/scripts.js
@@ -76,6 +76,8 @@ jQuery(document).ready(function($) {
 
   // Menu Functionality
 
+  $('.sticky-header').midnight();
+
   $('.sub-menu').hide();
   $('li.menu-item-has-children').append('<span class="sub-menu-icon genericon genericon-expand"></span>');
   $('.menu-mobile-container li.menu-item-has-children').click(function(){
